f3d4556c9ebfec255250deaae6fb5e42a8b1429b
Mariusz Mikulski <mariusz.mikulski4@outlook.com>
2023-02-21 22:10:31 +0100

    Added folder for project modules with header

---
+++ module/module.h
@@ -0,0 +1,41 @@
+/** @file module.h
+*
+* @author 		
+* Mariusz Mikulski	\n
+* Company: 	\n
+* Departament:	\n
+* @date		Feb 19, 2023
+* @version 	1.0.0
+* @copyright 	© 2022. All Rights Reserved.
+*
+* @brief brief description of module.h.
+*
+* @page module.h
+* @details Detail description of module.h.
+*
+*/
+
+/* Define to prevent recursive inclusion -----------------------------------------*/
+#ifndef _MODULE_H_
+#define _MODULE_H_
+
+/* Includes ----------------------------------------------------------------------*/
+
+/* Exported define ---------------------------------------------------------------*/
+/* Exported types ----------------------------------------------------------------*/
+/* Exported constants ------------------------------------------------------------*/
+/* Exported macro ----------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------- */
+/**
+* @brief Brief description of function
+*/
+/* Exported Object types ---------------------------------------------------------*/
+/* Exported Object constants -----------------------------------------------------*/
+/* Exported Object macro ---------------------------------------------------------*/
+/* Exported Object functions -----------------------------------------------------*/
+
+
+
+#endif /* _MODULE_H_ */
+
+/*-------------------------------END OF FILE--------------------------------------*/

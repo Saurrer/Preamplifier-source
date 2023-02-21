3f8f1c7d493d86de16305d9f47bcbd52ca9cb189
Mariusz Mikulski <mariusz.mikulski4@outlook.com>
2023-02-21 22:13:56 +0100

    Created HMI module

---
+++ module/HMI/inc/HMI.h
@@ -0,0 +1,49 @@
+/** @file HMI.h
+*
+* @author 		
+* Mariusz Mikulski	\n
+* Company: 	\n
+* Departament:	\n
+* @date		Feb 21, 2023
+* @version 	1.0.0
+* @copyright 	© 2022. All Rights Reserved.
+*
+* @brief brief description of HMI.h.
+*
+* @page HMI.h
+* @details Detail description of HMI.h.
+*
+*/
+
+/* Define to prevent recursive inclusion -----------------------------------------*/
+#ifndef _HMI_H_
+#define _HMI_H_
+
+/* Includes ----------------------------------------------------------------------*/
+
+/* Exported define ---------------------------------------------------------------*/
+/* Exported types ----------------------------------------------------------------*/
+
+namespace HMI
+{
+
+
+} // namespace HMI
+
+/* Exported constants ------------------------------------------------------------*/
+/* Exported macro ----------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------- */
+/**
+* @brief Brief description of function
+*/
+
+/* Exported Object types ---------------------------------------------------------*/
+/* Exported Object constants -----------------------------------------------------*/
+/* Exported Object macro ---------------------------------------------------------*/
+/* Exported Object functions -----------------------------------------------------*/
+
+
+
+#endif /* _HMI_H_ */
+
+/*-------------------------------END OF FILE--------------------------------------*/
---
+++ module/HMI/src/HMI.cpp
@@ -0,0 +1,43 @@
+/** @file HMI.cpp
+*
+* @author 		
+* Mariusz Mikulski	\n
+* Company: 	\n
+* Departament:	\n
+* @date		Feb 21, 2023
+* @version 	1.0.0
+* @copyright 	© 2022. All Rights Reserved.
+*
+* @brief brief description of HMI.cpp.
+*
+* @page HMI.cpp
+* @details Detail description of HMI.cpp.
+*
+*/
+/* Includes ----------------------------------------------------------------------*/
+#include <stdint.h>
+
+#include <cmsis_gcc.h>
+#include <stm32f091xc.h>
+
+#include "../inc/HMI.h"
+/* Private typedef ---------------------------------------------------------------*/
+namespace HMI
+{
+
+} // namespace HMI
+
+/* Private define ----------------------------------------------------------------*/
+/* Private macro -----------------------------------------------------------------*/
+/* Private variables -------------------------------------------------------------*/
+/* Private function prototypes ---------------------------------------------------*/
+/*
+  Function:
+  Input Data:
+  Action:
+*/
+
+/* Private functions -------------------------------------------------------------*/
+
+
+/*-------------------------------END OF FILE--------------------------------------*/

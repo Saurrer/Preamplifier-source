95072fddb968de91cc97acb858796bdb3e8aa855
Mariusz Mikulski <mariusz.mikulski4@outlook.com>
2023-02-28 19:24:34 +0100

    Updated WS2812B comm - DMI handle, sys_clk boost from 8MHz to 48MHz

--- module/HMI/knob/led/interface/WS2812B/src/ws2812b.cpp
+++ module/HMI/knob/led/interface/WS2812B/src/ws2812b.cpp
@@ -35,11 +35,12 @@
 #define WS2812B_MID_Pos								(3U)
 #define WS2812B_LSB_Pos								(6U)
 
+#define WS2812B_LED_COUNT							(5U)
 /* Private macro -----------------------------------------------------------------*/
 /* Private variables -------------------------------------------------------------*/
 int8_t WS2812B::init_flag {0};
 
-volatile uint8_t led_table[6][8];
+volatile uint8_t led_table[WS2812B_LED_COUNT][8];
 uint8_t WS2812B_UART_BUFFER_DEFAULT_VALUE = 0x92;
 /* Private function prototypes ---------------------------------------------------*/
 /*
@@ -57,7 +58,7 @@ WS2812B::DMA_peripheral_conf(DMA_Channel_TypeDef * pDMA_Channel)
    */
   DMA_select_channel(DMA2, CHANNEL_1, 0b1001);
   DMA_set_data_transfer_dir(pDMA_Channel, DMA_DATA_TRANSFER_M2P);
-  DMA_set_number_of_data_to_transfer(pDMA_Channel, 6*8);
+  DMA_set_number_of_data_to_transfer(pDMA_Channel, (WS2812B_LED_COUNT * 8));
 
   DMA_set_memory_configuration(pDMA_Channel, (uint32_t*) led_table, 0, 1);
   DMA_set_peripheral_configuration(pDMA_Channel, (uint32_t*) &USART2->TDR, 0, 0);
@@ -74,7 +75,7 @@ WS2812B::DMA_mem_conf(DMA_Channel_TypeDef * pDMA_Channel)
    */
   //DMA_select_channel(DMA2_Channel1, CHANNEL_1, 0b1001);
   DMA_set_memory2memory_mode(pDMA_Channel);
-  DMA_set_number_of_data_to_transfer(pDMA_Channel, 6*8);
+  DMA_set_number_of_data_to_transfer(pDMA_Channel, (WS2812B_LED_COUNT * 8));
 
   DMA_set_memory_configuration(pDMA_Channel, (uint32_t*) led_table, 0, 1);				/**< source */
   DMA_set_peripheral_configuration(pDMA_Channel, (uint32_t*) &WS2812B_UART_BUFFER_DEFAULT_VALUE, 0, 0);	/**< destination */
@@ -102,8 +103,15 @@ WS2812B::Init(void)
 void
 WS2812B::resetBuffer(void)
 {
-  DMA2_Channel1->CCR |= DMA_CCR_EN;		/**< reset buffer to default value */
-  while(DMA2_Channel1->CCR & DMA_CCR_EN);
+  DMA1_Channel7->CCR |= DMA_CCR_EN;		/**< reset buffer to default value */
+  while(!(DMA1->ISR & DMA_ISR_TCIF7));		/**< wait till transfer is finished */
+
+  DMA1->IFCR |= DMA_IFCR_CTCIF7;		/**< clear flag */
+
+  DMA1_Channel7->CCR &= ~(DMA_CCR_EN);
+  DMA_set_number_of_data_to_transfer(DMA1_Channel7, (WS2812B_LED_COUNT * 8));
+
+  //while(DMA1_Channel7->CCR & DMA_CCR_EN);	/**< IMPORTANT: after successful transfer bit DMA_EN stay 1. */
 }
 
 void
@@ -112,7 +120,7 @@ WS2812B::AddDataToBuffer(volatile uint8_t (*buffer)[8], uint32_t GRB_data)
   uint8_t i, j, k;
   uint32_t temp_grb_data = (~GRB_data & 0x00ffffff);
 
-  for(i = 0; i < 6; i++)	/**< 6 - ilosc ledow */
+  for(i = 0; i < WS2812B_LED_COUNT; i++)	/**< WS2812B_LED_COUNT - ilosc ledow */
     {
       for(j = 0, k = 24 - 1; j < 8; j++, k-=3)
         {
@@ -129,7 +137,13 @@ WS2812B::send(uint32_t grb_data)
   AddDataToBuffer(led_table, grb_data);
 
   DMA2_Channel1->CCR |= DMA_CCR_EN;
-  while(DMA2_Channel1->CCR & DMA_CCR_EN);
+  while(!(DMA2->ISR & DMA_ISR_TCIF1));		/**< wait till transfer is finished */
+
+  DMA2->IFCR |= DMA_IFCR_CTCIF1;		/**< clear flag */
+  DMA2_Channel1->CCR &= ~(DMA_CCR_EN);
+  DMA_set_number_of_data_to_transfer(DMA2_Channel1, (WS2812B_LED_COUNT * 8));
+
+  //while(DMA2_Channel1->CCR & DMA_CCR_EN);	/**< IMPORTANT: after successful transfer bit DMA_EN stay 1. */
 }
 
 void
--- startup/src/SystemStartup.c
+++ startup/src/SystemStartup.c
@@ -10,31 +10,90 @@
 *
 * @brief brief description of file.
 *
-* @page make_page_for_this_file_in_doxygen
+* @page SystemStartup.cpp
 *
 * @details Detail description of file.
 *
 */
-
+/* Includes ----------------------------------------------------------------------*/
 
 #include <stdint.h>
 
 #include <cmsis_gcc.h>
 #include <stm32f091xc.h>
 
-void SystemStartup(void);
+#include "../inc/SystemStartup.h"
 
+/* Private define ----------------------------------------------------------------*/
+#define OSC_IN_PORT	GPIOF
+#define OSC_IN_PIN	PF0
+
+#define OSC_OUT_PORT	GPIOF
+#define OSC_OUT_PIN	PF1
+
+/* Private function prototypes ---------------------------------------------------*/
+
+void SystemStartup(void);
+void set_external_hse(void);
+
+/* Private functions -------------------------------------------------------------*/
 void SystemStartup(void)
 {
-  RCC->AHBENR |=  RCC_AHBENR_GPIOAEN ;
+  RCC->AHBENR |=  RCC_AHBENR_GPIOAEN 	|
+		  RCC_AHBENR_DMA1EN	|	/** ws2812b interface */
+		  RCC_AHBENR_DMA2EN  	;	/** ws2812b interface */
 
   RCC->APB1ENR |= RCC_APB1ENR_TIM7EN	|	/** delay */
+		  RCC_APB1ENR_USART2EN	|	/** ws2812b interface */
 		  RCC_APB1ENR_TIM2EN	;	/** timer used by knob */
 
   //RCC->APB1ENR |= RCC_APB1ENR_USART2EN;	//logs
 
+  set_external_hse();
+
   __DSB();
   __ISB();
+}
+
+
+void set_external_hse(void)
+{
+  RCC->AHBENR |= RCC_AHBENR_GPIOFEN;
+/*
+ * When the HSE or LSE oscillator is switched ON (by setting the HSEON or LSEON bit in the
+ * RCC_CSR register) the oscillator takes control of its associated pins and the GPIO
+ * configuration of these pins has no effect
+ */
+  FLASH->ACR |= FLASH_ACR_PRFTBE;		/** enable prefetch buffer */
+
+  /* GPIO remap */
+
+  RCC->CR |= RCC_CR_CSSON;			/** enable Clock security system */
+  __DSB();
+  __ISB();
+
+  RCC->CR |= RCC_CR_HSEON;
+  while(!(RCC->CR & RCC_CR_HSERDY));		/** if hse is ready */
+
+  /* PLL */
+  RCC->CFGR2 = (1U << RCC_CFGR2_PREDIV_Pos);	/** PREDIV - div2 */
+  RCC->CFGR |= (2U << RCC_CFGR_PLLSRC_Pos) |	/** PLLSRC - HSE */
+	       (10U << RCC_CFGR_PLLMUL_Pos) ;	/** PLLMUL - x12 */
+  __DSB();
+  RCC->CR |= RCC_CR_PLLON;
+  while(!(RCC->CR & RCC_CR_PLLRDY));
+
+  /* FLASH latency */
+  FLASH->ACR |= (1U << FLASH_ACR_LATENCY_Pos);	/** one wait state */
+  while(!(FLASH->ACR & (1U << FLASH_ACR_LATENCY_Pos)));
+
+  //RCC->CFGR |= (1U << RCC_CFGR_SW_Pos);	/** switch sys clk source to hse */
+  //while(!((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_HSE));
+
+  RCC->CFGR |= (2U << RCC_CFGR_SW_Pos);		/** switch sys clk source to PLL clk*/
+  while(!((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_PLL));
+
+  RCC->CFGR3 |= (1U << RCC_CFGR3_USART2SW_Pos);	/** set usart2 clock to sys clk */
 }
 
 

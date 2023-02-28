fdb6bfa059be5c440b5f9d164caa258ab2bf755b
Mariusz Mikulski <mariusz.mikulski4@outlook.com>
2023-02-28 21:18:56 +0100

    Added config file and moved defined CPU_FREQ there

---
+++ config.h
@@ -0,0 +1,44 @@
+/** @file config.h
+*
+* @author 		
+* Mariusz Mikulski	\n
+* Company: 	\n
+* Departament:	\n
+* @date		Feb 28, 2023
+* @version 	1.0.0
+* @copyright 	© 2023. All Rights Reserved.
+*
+* @brief brief description of config.h.
+*
+* @page config.h
+* @details Detail description of config.h.
+*
+*/
+
+/* Define to prevent recursive inclusion -----------------------------------------*/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* Exported define ---------------------------------------------------------------*/
+#define CPU_FREQUENCY				(48000000U)
+
+
+/* Exported types ----------------------------------------------------------------*/
+//extern uint16_t somevariable; /**< Line description of somevariable */
+/* Exported constants ------------------------------------------------------------*/
+/* Exported macro ----------------------------------------------------------------*/
+/* Exported functions ----------------------------------------------------------- */
+/**
+* @brief Brief description of function
+*/
+
+/* Exported Object types ---------------------------------------------------------*/
+/* Exported Object constants -----------------------------------------------------*/
+/* Exported Object macro ---------------------------------------------------------*/
+/* Exported Object functions -----------------------------------------------------*/
+
+
+
+#endif /* _CONFIG_H_ */
+
+/*-------------------------------END OF FILE--------------------------------------*/

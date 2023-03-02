40b7c0cc9b166c41e5daf93307e122ed0e09bac6
Mariusz Mikulski <mariusz.mikulski4@outlook.com>
2023-03-02 20:27:23 +0100

    Moved definition of menu nodes from header to source file

--- module/HMI/menu/inc/menu.h
+++ module/HMI/menu/inc/menu.h
@@ -53,41 +53,24 @@ namespace HMI
     int8_t exeFunction(void);
 
   private:
-    NODE menu_0 = {.Name = MENU_MAIN_0, .pNext = &menu_1, .pPrevious = &menu_2, .pParent = NULL, .pChild = &sub_menu_0_1, .function = NULL};
-    //bez sensu robic node dla kazdego utworu - ToDo - trzeba sie zastanowic jak to rozwiazac
-        NODE sub_menu_0_1 = {.Name = "submenu_0_1",
-  			   .pNext = &sub_menu_0_2, .pPrevious = &sub_menu_0_3, .pParent = &menu_0, .pChild = NULL, .function = NULL};
-        NODE sub_menu_0_2 = {.Name = "submenu_0_2",
-  			   .pNext = &sub_menu_0_3, .pPrevious = &sub_menu_0_1, .pParent = &menu_0, .pChild = NULL, .function = NULL};
-        NODE sub_menu_0_3 = {.Name = "submenu_0_3",
-  			   .pNext = &sub_menu_0_1, .pPrevious = &sub_menu_0_2, .pParent = &menu_0, .pChild = NULL, .function = NULL};
+    static NODE menu_0;
+        static NODE sub_menu_0_1;
+        static NODE sub_menu_0_2;
+        static NODE sub_menu_0_3;
 
+    static NODE menu_1;
+	static NODE sub_menu_1_1;
+        static NODE sub_menu_1_2;
+        static NODE sub_menu_1_3;
+        static NODE sub_menu_1_4;
+        static NODE sub_menu_1_5;
+        static NODE sub_menu_1_6;
+        static NODE sub_menu_1_7;
+        static NODE sub_menu_1_8;
 
-    NODE menu_1 = {.Name = (const char *) MENU_MAIN_1, .pNext = &menu_2, &menu_0, NULL, &sub_menu_1_1, .function = NULL};
-        NODE sub_menu_1_1 = {.Name = "submenu_1_1",
-  			   .pNext = &sub_menu_1_2, .pPrevious = &sub_menu_1_8, .pParent = &menu_1, .pChild = NULL, .function = NULL};
-        NODE sub_menu_1_2 = {.Name = "submenu_1_2",
-  			   .pNext = &sub_menu_1_3, .pPrevious = &sub_menu_1_1, .pParent = &menu_1, .pChild = NULL, .function = &changeVolume};
-        NODE sub_menu_1_3 = {.Name = "submenu_1_3",
-  			   .pNext = &sub_menu_1_4, .pPrevious = &sub_menu_1_2, .pParent = &menu_1, .pChild = NULL, .function = &changeBass};
-        NODE sub_menu_1_4 = {.Name = "submenu_1_4",
-  			   .pNext = &sub_menu_1_5, .pPrevious = &sub_menu_1_3, .pParent = &menu_1, .pChild = NULL, .function = &changeTreble};
-        NODE sub_menu_1_5 = {.Name = "submenu_1_5",
-  			   .pNext = &sub_menu_1_6, .pPrevious = &sub_menu_1_4, .pParent = &menu_1, .pChild = NULL, .function = &changeFader_FR};
-        NODE sub_menu_1_6 = {.Name = "submenu_1_6",
-  			   .pNext = &sub_menu_1_7, .pPrevious = &sub_menu_1_5, .pParent = &menu_1, .pChild = NULL, .function = &changeFader_FL};
-        NODE sub_menu_1_7 = {.Name = "submenu_1_7",
-  			   .pNext = &sub_menu_1_8, .pPrevious = &sub_menu_1_6, .pParent = &menu_1, .pChild = NULL, .function = &changeFader_RR};
-        NODE sub_menu_1_8 = {.Name = "submenu_1_8",
-  			   .pNext = &sub_menu_1_1, .pPrevious = &sub_menu_1_7, .pParent = &menu_1, .pChild = NULL, .function = &changeFader_RL};
-
-
-    NODE menu_2 = {.Name = (const char *) MENU_MAIN_2, .pNext = &menu_0, &menu_1, NULL, &sub_menu_2_1, .function = NULL};
-        NODE sub_menu_2_1 = {.Name = "submenu_2_1",
-  			   .pNext = &sub_menu_2_2, .pPrevious = &sub_menu_2_2, .pParent = &menu_2, .pChild = NULL, .function = NULL};
-        NODE sub_menu_2_2 = {.Name = "submenu_2_2",
-  			   .pNext = &sub_menu_2_1, .pPrevious = &sub_menu_2_1, .pParent = &menu_2, .pChild = NULL, .function = NULL};
-
+    static NODE menu_2;
+	static NODE sub_menu_2_1;
+        static NODE sub_menu_2_2;
 
   };
 
--- module/HMI/menu/src/menu.cpp
+++ module/HMI/menu/src/menu.cpp
@@ -20,9 +20,43 @@
 #include "../inc/menu.h"
 /* Private typedef ---------------------------------------------------------------*/
 /* Private define ----------------------------------------------------------------*/
-#define NULL 	((void *)0)
+#define NULL 				(0)
 /* Private macro -----------------------------------------------------------------*/
 /* Private variables -------------------------------------------------------------*/
+NODE HMI::MENU::menu_0 = { .Name = MENU_MAIN_0,
+			   .pNext = &menu_1, .pPrevious = &menu_2, .pParent = NULL, .pChild = &sub_menu_0_1, .function = NULL};
+    NODE HMI::MENU::sub_menu_0_1 = { .Name = "submenu_0_1",
+				     .pNext = &sub_menu_0_2, .pPrevious = &sub_menu_0_3, .pParent = &menu_0, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_0_2 = { .Name = "submenu_0_2",
+				     .pNext = &sub_menu_0_3, .pPrevious = &sub_menu_0_1, .pParent = &menu_0, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_0_3 = { .Name = "submenu_0_3",
+				     .pNext = &sub_menu_0_1, .pPrevious = &sub_menu_0_2, .pParent = &menu_0, .pChild = NULL, .function = NULL};
+
+NODE HMI::MENU::menu_1 = { .Name = MENU_MAIN_1,
+			   .pNext = &menu_2, &menu_0, NULL, &sub_menu_1_1, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_1 = { .Name = "submenu_1_1",
+				     .pNext = &sub_menu_1_2, .pPrevious = &sub_menu_1_8, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_2 = { .Name = "submenu_1_2",
+				     .pNext = &sub_menu_1_3, .pPrevious = &sub_menu_1_1, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_3 = { .Name = "submenu_1_3",
+				     .pNext = &sub_menu_1_4, .pPrevious = &sub_menu_1_2, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_4 = { .Name = "submenu_1_4",
+				     .pNext = &sub_menu_1_5, .pPrevious = &sub_menu_1_3, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_5 = { .Name = "submenu_1_5",
+				     .pNext = &sub_menu_1_6, .pPrevious = &sub_menu_1_4, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_6 = { .Name = "submenu_1_6",
+				      .pNext = &sub_menu_1_7, .pPrevious = &sub_menu_1_5, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_7 = { .Name = "submenu_1_7",
+				      .pNext = &sub_menu_1_8, .pPrevious = &sub_menu_1_6, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_1_8 = { .Name = "submenu_1_8",
+				     .pNext = &sub_menu_1_1, .pPrevious = &sub_menu_1_7, .pParent = &menu_1, .pChild = NULL, .function = NULL};
+
+NODE HMI::MENU::menu_2 = {.Name = MENU_MAIN_2, .pNext = &menu_0, &menu_1, NULL, &sub_menu_2_1, .function = NULL};
+    NODE HMI::MENU::sub_menu_2_1 = { .Name = "submenu_2_1",
+				     .pNext = &sub_menu_2_2, .pPrevious = &sub_menu_2_2, .pParent = &menu_2, .pChild = NULL, .function = NULL};
+    NODE HMI::MENU::sub_menu_2_2 = { .Name = "submenu_2_2",
+				     .pNext = &sub_menu_2_1, .pPrevious = &sub_menu_2_1, .pParent = &menu_2, .pChild = NULL, .function = NULL};
+
 /* Private function prototypes ---------------------------------------------------*/
 /*
   Function:
